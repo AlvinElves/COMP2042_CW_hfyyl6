ebafeb5400dca9afaee50a25321eabd4dc36b5d5
AlvinElves <93361770+AlvinElves@users.noreply.github.com>
2021-12-08 15:37:43 +0800

    JUnit Test for Player Bar movement

---
+++ test/test/PlayerTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void playerBarMoveLeft() {
+        Player player = new Player((Point) new Point(300,430).clone(),150,10, new Rectangle(0,0,600,450));
+        Player.setDefMoveAmount(5);
+        player.playerBarMoveLeft();
+        assertEquals(-5, Player.getMoveAmount());
+    }
+
+    @Test
+    void playerBarMoveRight() {
+        Player player = new Player((Point) new Point(300,430).clone(),150,10, new Rectangle(0,0,600,450));
+        Player.setDefMoveAmount(3);
+        player.playerBarMoveRight();
+        assertEquals(3, Player.getMoveAmount());
+    }
+
+    @Test
+    void playerBarStop() {
+        Player player = new Player((Point) new Point(300,430).clone(),150,10, new Rectangle(0,0,600,450));
+        Player.setDefMoveAmount(5);
+        player.playerBarStop();
+        assertEquals(0, Player.getMoveAmount());
+    }
+}
